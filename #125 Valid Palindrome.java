f1d177607997506abfe9300630c4222fa1e13ed9
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-10-12 15:17:54 +0530

    #125

---
+++ #125 Valid Palindrome.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/valid-palindrome/
+class Solution {
+    public boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while(start < end) {
+            if(!Character.isLetterOrDigit(s.charAt(start))) {
+                start++;
+            }
+            else if(!Character.isLetterOrDigit(s.charAt(end))) {
+                end--;
+            }
+            else if(Character.toLowerCase(s.charAt(start))!=Character.toLowerCase(s.charAt(end))) {
+                return false;
+            }
+            else {
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+}
