d1069ded28534d7f0553641a5cfebd2be7245c1a
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-10-25 23:18:06 +0530

    #21

--- #21 Merege Two Sorted Lists.java
+++ #21 Merege Two Sorted Lists.java
@@ -10,36 +10,35 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        
-        ListNode head = new ListNode(0) ;
-        ListNode n = head;
-        while(l1!=null && l2!=null)
-        {
-            if(l1.val<=l2.val)
-            {
-                n.next=l1;
-                l1=l1.next;
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        ListNode newHead , n;
+        if(l1.val <= l2.val) {
+            newHead = l1;
+            l1 = l1.next;
+        }
+        else {
+            newHead = l2;
+            l2 = l2.next;
+        }
+        n = newHead;
+        while(l1 != null && l2!=null) {
+            if(l1.val <= l2.val) {
+                n.next = l1;
+                l1 = l1.next;
             }
-            else
-            {
-                n.next=l2;
-                l2=l2.next;
+            else {
+                n.next = l2;
+                l2 = l2.next;
             }
-            n=n.next;
+            n = n.next;
         }
-        while(l1!=null)
-        {
-           n.next=l1;
-           l1=l1.next;
-           n=n.next; 
+        if(l1 != null) {
+            n.next = l1;
         }
-        while(l2!=null)
-        {
-           n.next=l2;
-           l2=l2.next;
-           n=n.next; 
+        if(l2 != null) {
+            n.next = l2;
         }
-        
-        return head.next;
+        return newHead;
     }
 }
