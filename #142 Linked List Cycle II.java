7efd41fa5c43dc676cb92fef857273be522fab0e
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-10-27 19:03:32 +0530

    #142 Added Second Method

--- #142 Linked List Cycle II.java
+++ #142 Linked List Cycle II.java
@@ -11,14 +11,45 @@
  * }
  */
 //O(N) Space used.
+// public class Solution {
+//     public ListNode detectCycle(ListNode head) {
+//         HashSet<ListNode> set = new HashSet<>();
+//         while(head!=null) {
+//             if(set.contains(head)) return head;
+//             set.add(head);
+//             head = head.next;
+//         }
+//         return null;
+//     }
+// }
+
+// O(1) Space
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        HashSet<ListNode> set = new HashSet<>();
-        while(head!=null) {
-            if(set.contains(head)) return head;
-            set.add(head);
-            head = head.next;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null) {
+            slow = slow.next;
+            fast=fast.next.next;
+            if(slow == fast) break;
         }
-        return null;
+        if(fast == null || fast.next == null) return null;
+        slow = head;
+        while(slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
     }
 }
