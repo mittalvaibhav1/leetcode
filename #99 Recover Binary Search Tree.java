63e7a94133d3b598dd9c2c6e1106fd35b3fcd56b
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-11-01 02:03:03 +0530

    #99 Added M2

--- #99 Recover Binary Search Tree.java
+++ #99 Recover Binary Search Tree.java
@@ -45,3 +45,34 @@ class Solution {
         inorder(root.right);
     }
 }
+// M2
+
+class Solution {
+    private TreeNode first;
+    private TreeNode second;
+    private TreeNode pre;
+    public void recoverTree(TreeNode root) {
+        if(root==null) return;
+        first = null;
+        second = null;
+        pre = null;
+        inorder(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+    
+    private void inorder(TreeNode root){
+        if(root==null) return;
+        inorder(root.left);
+        
+        if(first==null && (pre==null ||pre.val>=root.val)){
+            first = pre;
+        }
+        if(first!=null && pre.val>=root.val){
+            second = root;
+        }
+        pre = root;
+        inorder(root.right);
+    }
+}
