a5196bc992f4e2d1f8824bece99c2b155ebef99c
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-11-09 00:56:34 +0530

    #559

---
+++ #559. Maximum Depth of N-ary Tree.java
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/maximum-depth-of-n-ary-tree/submissions/
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public int maxDepth(Node root) {
+        if(root == null) return 0;
+        int ans = 0;
+        Iterator<Node> it = root.children.iterator();
+        while(it.hasNext()) {
+            ans =  Math.max(ans , maxDepth(it.next()));
+        }
+        return 1 + ans;
+    }
+}
