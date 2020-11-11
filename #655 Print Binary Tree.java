7caf67fad879c799d48561a7e30355a7f8fc0d74
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-11-12 00:44:27 +0530

    #655

---
+++ #655 Print Binary Tree.java
@@ -0,0 +1,65 @@
+//https://leetcode.com/problems/print-binary-tree
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+//THIS QUESTION IS A WASTE OF TIME!
+public class Solution {
+    class Params {
+        Params(TreeNode n, int ii, int ll, int rr) {
+            root = n;
+            i = ii;
+            l = ll;
+            r = rr;
+        }
+        TreeNode root;
+        int i, l, r;
+    }
+    public List < List < String >> printTree(TreeNode root) {
+        int height = getHeight(root);
+        //System.out.println(height);
+        String[][] res = new String[height][(1 << height) - 1];
+        for (String[] arr: res)
+            Arrays.fill(arr, "");
+        List < List < String >> ans = new ArrayList < > ();
+        fill(res, root, 0, 0, res[0].length);
+        for (String[] arr: res)
+            ans.add(Arrays.asList(arr));
+        return ans;
+    }
+    public void fill(String[][] res, TreeNode root, int i, int l, int r) {
+        Queue < Params > queue = new LinkedList();
+        queue.add(new Params(root, 0, 0, res[0].length));
+        while (!queue.isEmpty()) {
+            Params p = queue.remove();
+            res[p.i][(p.l + p.r) / 2] = "" + p.root.val;
+            if (p.root.left != null)
+                queue.add(new Params(p.root.left, p.i + 1, p.l, (p.l + p.r) / 2));
+            if (p.root.right != null)
+                queue.add(new Params(p.root.right, p.i + 1, (p.l + p.r + 1) / 2, p.r));
+        }
+    }
+    public int getHeight(TreeNode root) {
+        Queue < TreeNode > queue = new LinkedList();
+        queue.add(root);
+        int height = 0;
+        while (!queue.isEmpty()) {
+            height++;
+            Queue < TreeNode > temp = new LinkedList();
+            while (!queue.isEmpty()) {
+                TreeNode node = queue.remove();
+                if (node.left != null)
+                    temp.add(node.left);
+                if (node.right != null)
+                    temp.add(node.right);
+            }
+            queue = temp;
+        }
+        return height;
+    }
+}
