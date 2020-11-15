6a715d2bf26a07b900c9ba9137997ce60a0516a1
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-11-16 02:26:48 +0530

    #590 M2

--- #590 N-ary Tree Postorder Traversal.java
+++ #590 N-ary Tree Postorder Traversal.java
@@ -17,7 +17,7 @@ class Node {
     }
 };
 */
-
+// M1 Recursive
 class Solution {
     List<Integer> res;
     public List<Integer> postorder(Node root) {
@@ -32,5 +32,49 @@ class Solution {
             helper(it.next());
         }
         res.add(root.val);
+    }
+}
+//M2 - iterative
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null) return res;
+        Stack<Node> st = new Stack<>();
+        st.push(root);
+        st.push(root);
+        while(!st.isEmpty()) {
+            root = st.pop();
+            if(!st.isEmpty() && st.peek() == root) {
+                for(int i = root.children.size() - 1 ; i >= 0 ; i--) {
+                    st.push(root.children.get(i));
+                    st.push(root.children.get(i));
+                }
+            }
+            else {
+                res.add(root.val);
+                root = null;
+            }
+            
+        }
+        return res;
     }
 }
