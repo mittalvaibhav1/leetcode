38145b4da57da48f7573eec37d40fae25afa78ad
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-11-15 23:22:29 +0530

    #145 M2

--- #145 Binary Tree Postorder Traversal.java
+++ #145 Binary Tree Postorder Traversal.java
@@ -28,3 +28,41 @@ class Solution {
         res.add(root.val);
     }
 }
+// M2 Iterative
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        Stack<TreeNode> st = new Stack<>();
+        while(!st.isEmpty() || root != null) {
+            while(root != null) {
+                st.push(root);
+                st.push(root);
+                root = root.left;
+            }
+            root = st.pop();
+            if(!st.isEmpty() && st.peek() == root) {
+               root = root.right; 
+            }
+            else {
+                res.add(root.val);
+                root = null;
+            }
+        }
+        return res;
+    }
+}
