6339bd1babccc4e0f5fb151c674a974a7ac92de6
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-11-15 22:58:35 +0530

    #589 M2

--- #589 N -ary Tree Preorder Traversal.java
+++ #589 N -ary Tree Preorder Traversal.java
@@ -17,7 +17,7 @@ class Node {
     }
 };
 */
-
+// M1 Recursive
 class Solution {
     List<Integer> res;
     public List<Integer> preorder(Node root) {
@@ -34,3 +34,38 @@ class Solution {
         }
     }
 }
+// M2 Iterative
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new LinkedList<>();
+        if(root == null) return res;
+        Stack<Node> st = new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            root = st.pop();
+            res.add(root.val);
+            for(int i = root.children.size() - 1 ; i >= 0 ; i--) {
+                st.push(root.children.get(i));
+            }
+        }
+        return res;
+    }
+} 
