19568762eaec20a259fb6bdc380adfddc0a7cb14
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-11-19 01:13:43 +0530

    #105 M2

--- #105 Construct Binary Tree from Preorder and Inorder Traversal.java
+++ #105 Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -38,3 +38,41 @@ class Solution {
         return root;
     }
 }
+// M2 - 1ms
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    HashMap<Integer, Integer> map;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        map = new HashMap<>();
+        for(int i = 0 ; i < inorder.length ; i++) {
+            map.put(inorder[i],i);
+        }
+        return helper(preorder,0,preorder.length - 1 ,0,inorder.length - 1);
+    }
+    public TreeNode helper(int[] preorder, int sp , int ep, int si , int ei) {
+        if(sp > ep) return null;
+        TreeNode root = new TreeNode(preorder[sp]);
+        int index = map.get(preorder[sp]);
+        int sizeLeft = index - si;
+        int sizeRight = ei - index;
+        int sr = ep - sizeRight + 1;
+        int el = sp + sizeLeft;
+        root.left = helper(preorder,sp+1,el,si,index - 1);
+        root.right = helper(preorder,sr,ep,index + 1 , ei);
+        return root;
+    }
+}
