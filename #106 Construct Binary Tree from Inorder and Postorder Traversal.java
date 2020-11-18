6e26a493a9f16bd7a0fbca295d3a06515ac1d2b0
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-11-19 01:28:11 +0530

    #106 M2

--- #106 Construct Binary Tree from Inorder and Postorder Traversal.java
+++ #106 Construct Binary Tree from Inorder and Postorder Traversal.java
@@ -38,3 +38,41 @@ class Solution {
         return root;
     }
 }
+//M2 - 1ms
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    HashMap<Integer,Integer> map;
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        map = new HashMap<>();
+        for(int i = 0 ; i < inorder.length ; i++) {
+            map.put(inorder[i],i);
+        }
+        return helper(postorder,0,inorder.length-1,0,postorder.length-1);
+    }
+    public TreeNode helper(int[] post, int si, int ei, int sp, int ep) {
+        if(sp > ep) return null;
+        TreeNode root = new TreeNode(post[ep]);
+        int index = map.get(post[ep]);
+        int sizeLeft = index - si;
+        int sizeRight = ei - index;
+        int el = sp + sizeLeft - 1;
+        int sr = ep - sizeRight;
+        root.left = helper(post,si,index-1,sp,el);
+        root.right = helper(post,index+1,ei,sr,ep-1);
+        return root;
+    }
+}
