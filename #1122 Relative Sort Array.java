8d374ca1d8e25056b344376a09150bff873107e8
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-11-22 00:44:03 +0530

    #1122 M2

--- #1122 Relative Sort Array.java
+++ #1122 Relative Sort Array.java
@@ -1,4 +1,5 @@
 //https://leetcode.com/problems/relative-sort-array/
+//M1
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
         HashMap<Integer,Integer> map = new HashMap<>();
@@ -24,6 +25,27 @@ class Solution {
         Collections.sort(tmp);
         for(int i : tmp) {
             arr1[k++] = i;
+        }
+        return arr1;
+    }
+}
+// M2
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] map = new int[1001];
+        for(int i = 0 ; i < arr1.length; i++) {
+           map[arr1[i]]++;
+        }
+        int k = 0;
+        for(int i : arr2) {
+            while(map[i]-- > 0) {
+                arr1[k++] = i;
+            }
+        }
+        for(int i = 0 ; i < 1001 ; i++) {
+            while(map[i]-- > 0) {
+                arr1[k++] = i;
+            }
         }
         return arr1;
     }
