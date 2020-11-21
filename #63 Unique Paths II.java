b5e66e5c5e64cbceba913418901647cb19abfdef
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-11-22 02:07:41 +0530

    #63 Optimized

--- #63 Unique Paths II.java
+++ #63 Unique Paths II.java
@@ -24,14 +24,8 @@ class Solution {
             for(int j = 1 ; j < n ; j++) {
                 if(obstacleGrid[i][j] == 1) 
                     dp[i][j] = 0;
-                else {
-                    if(obstacleGrid[i-1][j] != 1) {
-                        dp[i][j] += dp[i-1][j];
-                    }
-                    if(obstacleGrid[i][j-1] != 1){
-                        dp[i][j] += dp[i][j-1];
-                    }
-                }
+                else
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
         
