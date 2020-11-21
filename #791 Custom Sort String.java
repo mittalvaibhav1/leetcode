c23db1db34ae360cef7e20627c80eefc5ccc3dba
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-11-22 00:00:54 +0530

    #791 M2

--- #791 Custom Sort String.java
+++ #791 Custom Sort String.java
@@ -1,5 +1,6 @@
 
 //https://leetcode.com/problems/custom-sort-string/
+//M1
 class Solution {
     public String customSortString(String S, String T) {
         HashMap<Character,Integer> map = new HashMap<>();
@@ -14,6 +15,25 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         for(int i = 0 ; i< arr.length ; i++) {
             sb.append(arr[i]);
+        }
+        return sb.toString();
+    }
+}
+// M2
+class Solution {
+    public String customSortString(String S, String T) {
+        int[] count = new int[26];
+        for(char i : T.toCharArray()) count[i - 'a']++;
+        StringBuilder sb = new StringBuilder();
+        for(char i : S.toCharArray()) {
+            while(count[i-'a']-- > 0) {
+                sb.append(i);
+            }
+        }
+        for(int i = 0 ; i < 26;i++){
+            while(count[i]-- > 0) {
+                sb.append((char)(i + 'a'));
+            }
         }
         return sb.toString();
     }
