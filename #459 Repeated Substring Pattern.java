cf0c9ddb783dca5215c205119fe9a1e8d77f9f06
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-11-23 01:27:43 +0530

    #459 M2

--- #459 Repeated Substring Pattern.java
+++ #459 Repeated Substring Pattern.java
@@ -14,3 +14,21 @@ class Solution {
        return false; 
     }
 }
+// M2 - 15ms
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+       int len = s.length();
+       for(int i = len/2 ; i >=1 ; i--) {
+           if(len % i != 0) continue;
+           String rep = s.substring(0,i);
+           StringBuilder sb = new StringBuilder();
+           int k = len / i;
+           int j = 0;
+           while(j++ < k) {
+               sb.append(rep);
+           }
+           if(sb.toString().equals(s)) return true;
+       }
+       return false; 
+    }
+}
