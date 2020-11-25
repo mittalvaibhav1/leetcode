613a3a4c550604d6cef468f0f5bae37db2617eec
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-11-25 14:18:38 +0530

    #1

--- #1 Two Sum.java
+++ #1 Two Sum.java
@@ -1,20 +1,15 @@
 //https://leetcode.com/problems/two-sum/
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-       int [] ans  = new int[2]; // resultant array
-       Map<Integer,Integer> map = new HashMap<>(); //for hashing 
-        for(int i=0;i<nums.length;i++)
-        {
-            if(map.containsKey(target-nums[i]))
-            {
-                ans[0] = map.get(target-nums[i]);
-                ans[1] = i;
-                break;// solution found in O(n) time and space complexity
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0 ; i < nums.length ; i++) {
+            if(map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target - nums[i]) , i};
             }
-            else
+            else {
                 map.put(nums[i],i);
+            }
         }
-        return ans;
+        return null;
     }
 }
