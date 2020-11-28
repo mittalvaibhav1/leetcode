a4b77f4854ff954805f81dc52ed9cd588f47a311
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-11-28 13:21:17 +0530

    #560

--- #560 Subarray Sum Equals K.java
+++ #560 Subarray Sum Equals K.java
@@ -1,25 +1,17 @@
 class Solution {
-    public boolean canPartition(int[] nums) {
-        int totalSum = 0;
-        // find sum of all array elements
-        for (int num : nums) {
-            totalSum += num;
-        }
-        // if totalSum is odd,it cannot be partitioned into equal sum subset
-        if (totalSum % 2 != 0) return false;
-        int subSetSum = totalSum / 2;
-        int n = nums.length;
-        boolean dp[][] = new boolean[n + 1][subSetSum + 1];
-        dp[0][0] = true;
-        for (int i = 1; i <= n; i++) {
-            int curr = nums[i - 1];
-            for (int j = 0; j <= subSetSum; j++) {
-                if (j < curr)
-                    dp[i][j] = dp[i - 1][j];
-                else
-                    dp[i][j] = dp[i - 1][j] || (dp[i - 1][j - curr]);
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int sum = 0;
+        int ans = 0;
+        map.put(0,1);
+        for(int i = 0 ; i < nums.length ; i++) {
+            sum += nums[i];
+            if(map.containsKey(sum - k)) {
+                ans += map.get(sum - k);
             }
+            map.put(sum , map.getOrDefault(sum , 0) + 1);
         }
-        return dp[n][subSetSum];
+        //System.out.println(map);
+        return ans;
     }
 }
