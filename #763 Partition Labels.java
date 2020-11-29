3749871c0dd36dd04b8aef44c675e5bbd5af1b35
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-11-29 16:11:08 +0530

    #763 M2

--- #763 Partition Labels.java
+++ #763 Partition Labels.java
@@ -1,4 +1,5 @@
 //https://leetcode.com/problems/partition-labels/
+// M1 5ms
 class Solution {
     public List<Integer> partitionLabels(String S) {
         List<Integer> res = new LinkedList<>();
@@ -10,6 +11,27 @@ class Solution {
         int len  = 0;
         for(int i  = 0 ; i < S.length() ; i++) {
             dist  = Math.max(map.get(S.charAt(i)) , dist);
+            len++;
+            if(dist == i) {
+                res.add(len);
+                len = 0;
+            }
+        }
+        return res;
+    }
+}
+//M2 3ms
+class Solution {
+    public List<Integer> partitionLabels(String S) {
+        List<Integer> res = new LinkedList<>();
+        int[] index = new int[26];
+        for(int i  = 0 ; i < S.length() ; i++) {
+            index[S.charAt(i) - 'a'] = i;
+        }
+        int dist = 0;
+        int len  = 0;
+        for(int i  = 0 ; i < S.length() ; i++) {
+            dist  = Math.max(index[S.charAt(i) - 'a'] , dist);
             len++;
             if(dist == i) {
                 res.add(len);
