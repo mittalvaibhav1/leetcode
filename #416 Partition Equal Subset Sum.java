466128823758cb74ab11e089bcfa5221b3f1725d
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-11-29 17:35:30 +0530

    #416 M2

--- #416 Partition Equal Subset Sum.java
+++ #416 Partition Equal Subset Sum.java
@@ -1,4 +1,5 @@
 //https://leetcode.com/problems/partition-equal-subset-sum/
+//M1 11ms
 class Solution {
     Boolean[][] memo;
     public boolean canPartition(int[] nums) {
@@ -15,5 +16,30 @@ class Solution {
         boolean res = helper(nums,target - nums[idx] , idx -1) || helper(nums,target, idx -1);
         memo[idx][target] = res;
         return res;
+    }
+}
+//M2 3ms
+class Solution {
+    Boolean[][] memo;
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for(int num : nums) sum += num;
+        if(sum % 2 != 0) return false;
+        int target = sum / 2;
+        memo = new Boolean[nums.length + 1][target + 1];
+        return helper(nums , target , 0 , 0);
+    }
+    public boolean helper(int[] nums , int target , int sum , int idx) {
+        if(target == sum) return true;
+        if(sum > target) return false;
+        if(memo[idx][target] != null) return memo[idx][target];
+        for(int i = idx ; i < nums.length ; i++) {
+            if(sum + nums[i] <= target) {
+                boolean res = helper(nums,target,sum + nums[i],i + 1);
+                memo[idx][target] = res;
+                if(res) return res;
+            }
+        }
+        return false;
     }
 }
