f6fa52b87803fc3a747c603ebf00786102959c79
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-12-05 17:08:41 +0530

    #605

---
+++ #605 Can Place Flowers.java
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/can-place-flowers/
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int possible = 0;
+        for(int i  = 0 ; i < flowerbed.length ; i++) {
+            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.length -1 || flowerbed[i+1] == 0)) {
+                flowerbed[i] = 1;
+                possible++;
+            }
+        }
+        return possible >= n;
+    }
+}
