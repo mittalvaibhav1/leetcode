cef1fdfd708a26a474fc7941663a4528591e8322
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-12-08 21:44:54 +0530

    #1010

---
+++ #1010 Pais of Songs With Total Durations Divisible by 60.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/
+class Solution {
+    public int numPairsDivisibleBy60(int[] time) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i : time) {
+            map.put(i % 60 , map.getOrDefault(i % 60, 0) + 1);
+        }
+        int count = 0;
+        for(int i = 0 ; i < time.length ; i++) {
+            int rem = time[i] % 60;
+            int val = map.get(rem);
+            if(val == 1) map.remove(rem);
+            else map.put(rem,val-1);
+            int req = 60 - rem;
+            if(map.containsKey(req % 60)) {
+                count += map.get(req % 60);
+            }
+        }
+        return count;
+    }
+}
