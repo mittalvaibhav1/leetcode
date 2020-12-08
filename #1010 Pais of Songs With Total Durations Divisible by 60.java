dc0d3c7ee78c7de4c269a306d504fdf0dd0c9106
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-12-08 21:51:06 +0530

    #1010 M3

--- #1010 Pais of Songs With Total Durations Divisible by 60.java
+++ #1010 Pais of Songs With Total Durations Divisible by 60.java
@@ -37,3 +37,17 @@ class Solution {
         return count;
     }
 }
+//M3 2ms
+class Solution {
+    public int numPairsDivisibleBy60(int[] time) {
+        int[] map = new int[60];
+        int count = 0;
+        for(int i = 0 ; i < time.length ; i++) {
+            int rem = time[i] % 60;
+            int req = 60 - rem;
+            count += map[req % 60];
+            map[rem]++;
+        }
+        return count;
+    }
+}
