bf7433e49741f04f7d21164ebd1a954bf75c7337
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-12-14 03:00:02 +0530

    #775

---
+++ #775 Global and Local Inversions.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/global-and-local-inversions/
+class Solution {
+    public boolean isIdealPermutation(int[] A) {
+        int local = 0;
+        int global = 0;
+        int[] cache = new int[A.length];
+        cache[A[0]]++;
+        for(int i = 1 ; i < A.length ; i++) {
+            global += getCount(cache,A[i]);
+            local += A[i-1] > A[i] ? 1 : 0;
+            cache[A[i]]++;
+        }
+        return local == global;
+    }
+    public int getCount(int[] cache, int val) {
+        int count = 0;
+        for(int i = val + 1 ; i < cache.length ; i++) {
+            count += cache[i];
+        }
+        return count;
+    }
+}
