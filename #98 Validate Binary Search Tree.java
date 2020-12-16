37b5f24c18a7390fb62f44be2fddd794333478fe
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-12-17 02:15:22 +0530

    #98 M2

--- #98 Validate Binary Search Tree.java
+++ #98 Validate Binary Search Tree.java
@@ -33,3 +33,32 @@ class Solution {
     }
     
 }
+//M2
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Integer prev = null;
+    public boolean isValidBST(TreeNode root) {
+        return helper(root);
+    }
+    public boolean helper(TreeNode root) {
+        if(root == null) return true;
+        boolean left = helper(root.left);
+        if(prev!= null && root.val <= prev) return false;
+        prev = root.val;
+        return left && helper(root.right);
+    }
+}
